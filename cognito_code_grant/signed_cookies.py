05d38456278df465a447a25ca4df3e511f335faf
Mario Brito <mario.brito@devoteam.com>
2020-01-23 13:30:44 +0000

    tweaking getattr instead

--- cognito_code_grant/signed_cookies.py
+++ cognito_code_grant/signed_cookies.py
@@ -108,10 +108,7 @@ class SignedCookiesMiddleware(object):
 
     def __call__(self, request):
         response = self.get_response(request)
-        try:
-            if getattr(request, 'user') and request.user.is_authenticated:
-                add_signed_cookies(response)
-        except:
-            pass
+        if getattr(request, 'user', None) and request.user.is_authenticated:
+            add_signed_cookies(response)
         return response
 
